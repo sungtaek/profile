19e35e6f3b5acedc40b4da95527b252e00fede12
sungtaek <leesungtaek@gmail.com>
2025-05-01 13:36:29 +0900

    update

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter"; // Import Router as WouterRouter
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,10 +6,12 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
 
-function Router() {
+// Rename the custom Router function
+function AppRoutes() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      {/* The NotFound route will now correctly handle paths relative to the base */}
       <Route component={NotFound} />
     </Switch>
   );
@@ -20,7 +22,10 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        {/* Use WouterRouter with the base path */}
+        <WouterRouter base="/profile">
+          <AppRoutes />
+        </WouterRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
